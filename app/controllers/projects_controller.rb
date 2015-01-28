f8a4fca14ec8c7d9911b4bd1ff3eae5391d15434
Yushadha Bogoda <yushadha@gmail.com>
2015-01-27 20:52:42 -0800

    made some CRUD for project: delete, edit

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,6 +3,8 @@ class ProjectsController < ApplicationController
                   only: [:show, :edit, :update, :destroy]
   
   def new
+    @project = Project.new
+    render :new
   end
 
   def index
@@ -10,6 +12,13 @@ class ProjectsController < ApplicationController
   end
 
   def create
+    @project = Project.new project_params
+    #@project.user = current_user
+    if project.save
+      redirect_to @project, notice: "Project created successfully"
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,15 +28,26 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    if @project.update project_params
+      redirect_to @project, notice: "Project updated successfully!"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @project.destroy
+    redirect_to projects_path, notice: "Project has been sucessfully removed"
   end
 
   private
 
   def find_project
     @project = Project.find params[:id]
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :due_date)
   end
 
 end
